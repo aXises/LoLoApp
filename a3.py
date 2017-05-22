e559e36c4b32b0a4f0040156653204b444c7525b
aXises <leletonyli.13944@yahoo.com.au>
2017-05-22 23:00:47 +1000

    Status bar label now gets the correct game name

--- a3.py
+++ a3.py
@@ -26,20 +26,18 @@ __version__ = "1.0.2"
 
 class LoloApp(BaseLoloApp):
 
+    GAME_NAME = None
+
     def __init__(self, master, game):
+
+        GAME_NAME = game.GAME_NAME
+        self.set_game(GAME_NAME)
 
         self._LoloLogo = LoloLogo(master)
         self._LoloLogo.pack(side=tk.TOP, anchor="n", expand=True, fill=tk.BOTH)
 
         self._StatusBar = StatusBar(master)
         self._StatusBar.pack(side=tk.TOP, fill=tk.X, padx=10, pady=5)
-
-        super().__init__(master)
-
-        self._master = master
-        self._game = game
-
-        master.title('Lolo :: %s Mode' % self._game.GAME_NAME)
 
         menubar = tk.Menu(master)
         master.config(menu=menubar)
@@ -49,22 +47,43 @@ class LoloApp(BaseLoloApp):
         dropdown.add_command(label="New Game", command=self.reset)
         dropdown.add_command(label="Exit", command=master.destroy)
 
+        master.title('Lolo :: %s Mode' % GAME_NAME)
+
+        super().__init__(master)
+
+
+    @classmethod
+    def get_game(cls):
+        return cls.GAME_NAME
+
+    @classmethod
+    def set_game(cls, game_mode):
+        cls.GAME_NAME = game_mode
+
+    def get_score(self):
+        pass
+
+    def set_score(self, score):
+        pass
+
     def reset(self):
         pass
+
 
 class StatusBar(tk.Frame):
 
     def __init__(self, parent):
         super().__init__(parent)
 
-        Game = tk.Label(self, text="game")
+        Game = tk.Label(self, text=LoloApp.get_game())
         Game.pack(side=tk.LEFT)
+        self.set_gamemode()
 
         Score = tk.Label(self, text="score")
         Score.pack(side=tk.RIGHT)
 
-    def set_game(self, game_mode):
-        pass
+    def set_gamemode(self):
+        print(LoloApp.get_game())
 
     def set_score(self, score):
         pass
